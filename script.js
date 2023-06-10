9d993106269d73a4c03e3d626b8fd0ad3349bc33
David <dnartey@kth.se>
2023-06-10 10:34:08 +0200

    Added manifest.json
    Tested and added manifest.json

--- script.js
+++ script.js
@@ -9,6 +9,32 @@ const options = {
 
 const response = await fetch('https://concerts-artists-events-tracker.p.rapidapi.com/venue/past?name=Hollywood%20bowl&minDate=2022-01-25&maxDate=2023-01-30&page=1',options);
 const record = await response.json()
-console.log('record', record)
+document.getElementById("Events").innerHTML = record.data.map(item => `<li>${item.name}</li>`).join('');
+
 }
 fetchData();
+
+
+/**
+ * Upcoming Events in Stockholm
+Dynamic chart 2022
+IShowSpeed
+The Smashing Pumpkins
+Adassa
+Adassa
+Zac Brown Band
+The Who
+Danny Elfman
+Danny Elfman
+AUDACY'S 9th Annual WE CAN SURVIVE 2022
+Florence + The Machine
+Florence + The Machine
+Maren Morris
+RÜFÜS DU SOL
+New Order
+New Order
+Yeah Yeah Yeahs
+Wu-Tang Clan
+Stevie Nicks
+Jack Johnson
+ */
